24e2c5b68aa9c2fd9073685f4334a5eae5d472a6
danielgynn <danielgynn@hotmail.co.uk>
2017-06-01 10:52:26 +0100

    Write basic express server

---
+++ server/server.js
@@ -0,0 +1,32 @@
+'use strict'
+
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var app = express();
+var router = express.Router();
+
+var port = process.env.API_PORT || 3001;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
+});
+
+router.get('/', function(req, res) {
+  res.json({ message: 'API is running!'});
+});
+
+app.use('/api', router);
+
+app.listen(port, function() {
+  console.log(`api running on port ${port}`);
+});
