94b8c0508f113d58240b235dedf1f5c255a759cc
danielgynn <danielgynn@hotmail.co.uk>
2017-06-01 13:31:44 +0100

    Get posts for production url

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import PostArea from './components/PostArea';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <Header title='React CRUD Blog' />
-        <PostArea url='http://localhost:3001/api/posts' pollInterval={2000} />
-      </div>
-    );
+    if (process.env.NODE_ENV === 'development') {
+      return (
+        <div>
+          <Header title='React CRUD Blog' />
+          <PostArea url='http://localhost:3001/api/posts' pollInterval={2000} />
+        </div>
+      );
+    } else if (process.env.NODE_ENV === 'production') {
+      return (
+        <div>
+          <Header title='React CRUD Blog' />
+          <PostArea url='https://react-crud-blog.herokuapp.com/api/posts' pollInterval={2000} />
+        </div>
+      );
+    }
   }
 }
 
